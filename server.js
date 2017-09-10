f5367eea2af59464c1a859f0ccb74e95234db76d
Noah Kim <noahbkim@gmail.com>
2017-09-10 10:58:39 -0400

    Add more logging information while debugging

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const passport = require('passport');
 const passportLocal = require('passport-local');
-// const expressSession = require('express-session');
+const expressSession = require('express-session');
 const methodOverride = require('method-override');
 const console = require('console');
 const mongoose = require('mongoose');
 
-// Webserver configuration
+// Web server configuration
 const port = process.env.PORT || 8000;
 
 // Application configuration
@@ -18,18 +18,21 @@ console.log('Created web server...');
 // Content type
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+console.log("Set up server communication...");
 
 // Session stuff
 app.use(cookieParser());
 app.use(expressSession({ secret: 'secret!', resave: false, saveUninitialized: false }));
 app.use(passport.initialize());
 app.use(passport.session());
+console.log("Initialized authentication...");
 
 // Authentication
-const auth = require('./server/accounts');
+const auth = require('./server/auth');
 passport.use(new passportLocal.Strategy(auth.Account.authenticate()));
 passport.serializeUser(auth.Account.serializeUser());
 passport.deserializeUser(auth.Account.deserializeUser());
+console.log("Loaded user models...");
 
 // Miscellaneous details
 app.use(express.static(__dirname + '/dist'));
@@ -39,6 +42,7 @@ console.log('Loaded plugins...');
 // Database initialization
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/scibowl', { useMongoClient: true });
+console.log("Connected to database...");
 
 // Direct API requests to server router
 const routes = require('./server/routes');
