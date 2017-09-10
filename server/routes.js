ef62d70a01b35bc2e4962cd6884bbc75a8a5a4db
Noah Singer <singerng@gmail.com>
2017-09-10 10:47:25 -0400

    more information in models

--- server/routes.js
+++ server/routes.js
@@ -1,16 +1,7 @@
 const express = require('express');
-const console = require('console');
 const router = express.Router();
 
-const models = require('./models');
-
-
-// router.get('/', (req, res) => {
-//   Task.find({}, (err, tasks) => {
-//     if (err) throw err;
-//     res.send(tasks);
-//   });
-// });
-
+const question = require('./routes/question');
+router.use('/question', question);
 
 module.exports = router;
