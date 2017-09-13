aaabd9633cd99e289f33d5053b0a4f23b36ea7fd
Noah Singer <singerng@gmail.com>
2017-09-12 23:21:31 -0400

    tryout frontend

--- server/models.js
+++ server/models.js
@@ -24,24 +24,35 @@ const multipleChoiceSchema = new Schema({
   }], required: true },
   answer: { type: Schema.Types.String, enum: game.MC_CHOICES, required: true }
 });
-const MultipleChoiceQuestion = Question.discriminator('MultipleChoice', multipleChoiceSchema);
+const MultipleChoiceQuestion = Question.discriminator('MultipleChoiceQuestion', multipleChoiceSchema);
 
 const shortAnswerSchema = new Schema({
   answer: { type: Schema.Types.String, required: true }
 });
-const ShortAnswerQuestion = Question.discriminator('ShortAnswer', shortAnswerSchema);
+const ShortAnswerQuestion = Question.discriminator('ShortAnswerQuestion', shortAnswerSchema);
 
 // Rounds
 const roundSchema = new Schema({
   owner: { type: Schema.Types.ObjectId, ref: 'User' },
-  questions: [{ type: Schema.Types.ObjectId, ref: 'Question', required: true }]
-});
+  questions: [{
+    question: { type: Schema.Types.ObjectId, ref: 'Question', required: true }
+  }]
+}, { discriminatorKey: 'kind '});
 const Round = mongoose.model('Round', roundSchema);
+
+const tryoutSchema = new Schema({
+  questions: [{
+    question: { type: Schema.Types.ObjectId, ref: 'MultipleChoiceQuestion', required: true },
+    time: { type: Schema.Types.Number }
+  }]
+});
+const TryoutRound = Round.discriminator('TryoutRound', tryoutSchema);
 
 module.exports = {
   User: User,
   Question: Question,
   MultipleChoiceQuestion: MultipleChoiceQuestion,
   ShortAnswerQuestion: ShortAnswerQuestion,
-  Round: Round
+  Round: Round,
+  TryoutRound: TryoutRound
 };
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,29 +3,37 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpModule } from '@angular/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
 import { QuestionListComponent } from './question/question.list.component';
 import { QuestionSearchComponent } from './question/question.search.component';
 import { QuestionViewComponent } from './question/question.view.component';
+import { TryoutComponent } from './tryout/tryout.component';
 
 const appRoutes: Routes = [
+  { path: '', component: IndexComponent },
   { path: 'question/search', component: QuestionSearchComponent },
-  { path: 'question/:id', component: QuestionViewComponent }
+  { path: 'question/:id', component: QuestionViewComponent },
+  { path: 'tryout', component: TryoutComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
+    IndexComponent,
     QuestionListComponent,
     QuestionSearchComponent,
-    QuestionViewComponent
+    QuestionViewComponent,
+    TryoutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
     HttpModule,
+    NgbModule.forRoot(),
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
