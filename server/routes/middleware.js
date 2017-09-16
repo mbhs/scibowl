a79a7a15ce726a0a34fa65b2e9616a0452cbb142
Noah Kim <noahbkim@gmail.com>
2017-09-16 19:58:13 -0400

    Added login checks

--- server/routes/middleware.js
+++ server/routes/middleware.js
@@ -3,7 +3,7 @@
  * Can be used as middleware; calls the next function if the user is
  * logged in and sends a 401 otherwise.
  */
-function assertUserAuthenticated(req, res, next) {
+module.exports.assertUserAuthenticated = function(req, res, next) {
   if (req.user) next();
   else res.status(401).send({});
-}
+};
